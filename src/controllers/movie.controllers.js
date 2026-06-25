719ddddc9d0a4d58ab3ec6b53ada1bd968f95047
VaninaCodes <vannilaresquin@gmail.com>
2026-06-25 12:09:33 -0300

    feat: se agregaron los controladores de movies

--- src/controllers/movie.controllers.js
+++ src/controllers/movie.controllers.js
@@ -1,8 +1,9 @@
 // validaciones
+import { extensions } from 'sequelize/lib/utils/validator-extras';
 import Movie from '../models/movie.model.js';
 
 // funcion auxiliar
-function validacionCampos ({ title, genre, duration, year, synopsis}){
+function validateMovieFields ({ title, genre, duration, year, synopsis}){
     // se devuelve un array de string con los errores
     const errors = [];
 
@@ -24,10 +25,10 @@ function validacionCampos ({ title, genre, duration, year, synopsis}){
     }
 
     // year obligatorio, entero de 4 digitos, entre 1888 y el año actual
-    const añoActual = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
     if (year === undefined || year === null || year === ''){
         errors.push('El campo year es obligatorio.')
-    } else if (typeof year !== 'number' || !Number.isInteger(year) || String(Math.abs(year)).length !== 4 || year < 1888 || year > añoActual) {
+    } else if (typeof year !== 'number' || !Number.isInteger(year) || String(Math.abs(year)).length !== 4 || year < 1888 || year > currentYear) {
         errors.push('El campo year debe ser un numero entero de 4 digitos (entre el año 1888 y el año actual).');
     }
 
@@ -37,3 +38,102 @@ function validacionCampos ({ title, genre, duration, year, synopsis}){
     }
     return errors;
 }
+
+// GET /api/movies
+// Obtener todas las peliculas
+export const getAllMovies = async (req, res) => {
+    try {
+        const movies = await Movie.findAll();
+        res.status(200).json(movies);
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error al obtener las peliculas', error: error.message});
+    }
+};
+
+// GET /api/movies/:id
+// Obtener pelicula por id
+export const getMoviesById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await Movie.findByPk(id);
+
+        if (!movie) {
+            return res.status(404).json({ message: `No se encontro ninguna pelicula con id: ${id}.`});
+        }
+        res.status(200).json(movie);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al buscar la pelicula.', error: error.message});
+    }
+};
+
+// POST /api/movies
+// Agregar una pelicula
+export const createMovie = async (req, res) => {
+    try {
+        const errors = validateMovieFields(req.body);
+        if (errors.length > 0) {
+            return res.status(400).json({ message: 'Datos invalidos.', errors});
+        }
+
+        const { title } = req.body;
+        //  busca si ya existe una pelicula con ese nombre
+        const exitingMovie = await Movie.findOne({ where: { title }});
+        if (exitingMovie) {
+            return res.status(400).json({ message: `Ya existe una pelicula registrada con el titulo: ${titulo}`});
+        }
+        // si no hay problemas crea la pelicula
+        const newMovie = await Movie.create(req.body);
+        res.status(201).json(newMovie);
+    } catch (error) {
+        res.status(500).json({ message:  'Error al crear la pelicula.', error: error.message});
+    }
+};
+
+// PUT /api/movies/:id
+// Actualizar una pelicula
+export const updateMovie = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const movie = await Movie.findByPk(id);
+
+        if (!movie) {
+            return res.status(404).json({ message: `No se encontro ninguna pelicula con id: ${id}.`});
+        }
+
+        const errors = validateMovieFields(req.body);
+        if (errors.length > 0) {
+            return res.status(400).json({ message: 'Datos invalidos', errors});
+        }
+        const { title } = req.body;
+        // busca si la pelicula existe
+        const existingMovie = await Movie.findOne({ where: { title } });
+        // se fija si el titulo no choca con un titulo existente
+        if (existingMovie && existingMovie.id !== movie.id) {
+            return res.status(400).json({ message: `Ya existe otra pelicula registrada con el titulo: ${title}.`});
+        }
+
+        await movie.update(req.body);
+        res.status(200).json(movie);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar la pelicula.', error: error.message});
+    }
+};
+
+// DELETE /api/movies/:id
+// Borrar una pelicula
+export const deleteMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await Movie.findByPk(id);
+        // busca si la pelicula existe
+        if(!movie){
+            return res.status(404).json({ message: `No se encontro ninguna pelicula con id: ${id}.`});
+        }
+        // si existe destroy()
+        await movie.destroy();
+        res.status(200).json({ message: `Pelicula con id: ${id} eliminada.`});
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar la pelicula.', error:error.message});
+    }
+};
